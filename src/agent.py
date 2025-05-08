a88251ffbe9f69858e9bf2374bbb34d724c99d45
averak <averak.jp@gmail.com>
2025-05-08 23:45:08 +0900

    hello exploit agent v2!

--- src/agent.py
+++ src/agent.py
@@ -3,6 +3,7 @@ from contextlib import AsyncExitStack
 from google.adk.agents import LlmAgent
 from google.adk.tools.agent_tool import AgentTool
 
+from agents.exploitation.agent import create_exploitation_agent
 from agents.intelligence_gathering import create_intelligence_gathering_agent
 from agents.vulnerability_analysis import create_vulnerability_analysis_agent
 from models import MODEL_GEMINI_2_0_FLASH
@@ -16,6 +17,10 @@ async def create_root_agent():
     common_exit_stack.push_async_exit(exit_stack.aclose)
 
     vulnerability_analysis_agent, exit_stack = await create_vulnerability_analysis_agent()
+    # noinspection PyTypeChecker
+    common_exit_stack.push_async_exit(exit_stack.aclose)
+
+    exploitation_agent, exit_stack = await create_exploitation_agent()
     # noinspection PyTypeChecker
     common_exit_stack.push_async_exit(exit_stack.aclose)
 
@@ -36,7 +41,7 @@ async def create_root_agent():
             - Intelligence Gathering: use: `intelligence_gathering_agent`
             - Threat Modeling
             - Vulnerability Analysis: use: `vulnerability_analysis_agent`
-            - Exploitation
+            - Exploitation: use: `exploitation_agent`
             - Post Exploitation
             - Reporting
         3. 各フェーズの専門エージェントを適切に起動し、結果を統合する。
@@ -50,8 +55,21 @@ async def create_root_agent():
         - 発見された脆弱性を明確に文書化し、再現手順と対策を提示する。
         - 情報セキュリティのベストプラクティスに基づいた提案を行う。
         - 調査結果を技術的正確さと実用的な対応策のバランスをとって提示する。
+        - スキャン、攻撃など、内部で実行している処理は、実行計画と実行ログを逐一報告すること。ただし、攻撃以外は許可不要で実行して良い。
+
+        ## ヒント
+
+        あなたは、WordPress の攻撃能力しか保有していません。
+        intelligence_gathering_agent tool で、
+
+        1. 開いているポートを、Fast Scan でスキャンする。
+        2. 各ポートで、動いているサービスを検出する。
+        3. WordPress が動いているポートに絞り込み、後続のステップに進む。
+
+        wordpress が動いている場合 Vulnerability Analysis (vulnerability_analysis_agent) -> Exploitation (exploitation_agent) を実行する。
+        レポートは、human-readable な形式で出力すること。
         """,
-        tools=[AgentTool(intelligence_gathering_agent), AgentTool(vulnerability_analysis_agent)],
+        tools=[AgentTool(intelligence_gathering_agent), AgentTool(vulnerability_analysis_agent), AgentTool(exploitation_agent)],
     )
     return agent, exit_stack
 
